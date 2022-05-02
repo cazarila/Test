aabb287d8c2ac19245e7e9d7d3ff7bffa3410338
Automation <Automation@KHILT-7836>
2022-05-02 15:01:54 +0500

    Test solution add

---
+++ Test/Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            a = 10;b = 3;
+            Console.WriteLine(a + b);
+
+            Console.ReadLine();
+        }
+    }
+}
